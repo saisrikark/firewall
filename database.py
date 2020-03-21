5f5e7d69313293f726f288f97e0331a9f143bc3b
ShashankBongale <shashankbongale@gmail.com>
2020-03-21 21:00:18 +0530

    db

--- database.py
+++ database.py
@@ -9,14 +9,15 @@ class SqlDatabase:
         self.password = None
         self.host = None
         self.current_database = None
-        self.connection = self.make_connection(username, password, host)
+        self.connection = self._make_connection(username, password, host)
 
-    def make_connection(self, username, password, host):
+    def _make_connection(self, username, password, host):
 
         connection = mysql.connector.connect(
             host=host,
             passwd=password,
-            user=username
+            user=username,
+            database="honeypot"
         )
 
         self.username = username
@@ -25,12 +26,17 @@ class SqlDatabase:
 
         return connection
 
+    def _execute_sql_command(self, command, args=None):
+
+        cursor = self.connection.cursor()
+        cursor.execute(command, args)
+        return cursor
+
     def create_database(self, database):
 
         try:
-            mycursor = self.connection.cursor()
             command = "CREATE DATABASE {0}".format(database)
-            mycursor.execute(command)
+            self._execute_sql_command(command)
 
         except mysql.connector.Error as err:
             print("Failed creating database: {}".format(err))
@@ -49,8 +55,8 @@ class SqlDatabase:
 
     @my_database.setter
     def my_database(self, db_name):
-        cursor = self.connection.cursor()
-        cursor.execute("SHOW DATABASES")
+
+        cursor = self._execute_sql_command("SHOW DATABASES")
         dbs = list(cursor)
 
         for i in dbs:
@@ -61,4 +67,11 @@ class SqlDatabase:
 
         raise Exception("No Database Named {0}".format(db_name))
 
-    
+    def insert_into_packets(self, values_tuple):
+
+        sql_command = 'INSERT INTO `packets`(`timestamp`, `src_ip`, `dest_ip`, `src_port`, `dest_port`, `src_mac`, `dest_mac`) VALUES ("1","1","1","1","1","1","1")'
+        r = self._execute_sql_command(sql_command)
+        print(r)
+
+
+
--- trial.py
+++ trial.py
@@ -2,8 +2,11 @@ from database import SqlDatabase
 
 sql_object = SqlDatabase("shashank", "shash27")
 
-#sql_object.create_database("trial_db1")
+#sql_object.create_database("trial_db2")
 
 sql_object.my_database = "honeypot"
 print(sql_object.my_database)
 
+values = ("1584770033.410457902", "a8:6b:ad:73:0c:e3", "a0:ab:1b:d8:39:eb",
+          "57646", "443", "192.168.0.109", "111.221.29.254")
+sql_object.insert_into_packets(values)
